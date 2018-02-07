8aea84bfdc265eeca073e9a4624ef5e357fdaa54
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-06 19:48:15 -0500

    created the layout for counter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div>
+    <button onClick = {() => this.setState({count: this.state.count + 1})}>
+      +
+    </button>
+    <button onClick = {() => this.setState({count: this.state.count - 1})}> -
+    </button>
+    <h1>
+      {this.state.count}
+    </h1>
+
+    </div>
     );
   }
 }

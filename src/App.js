ac4d75df32c602ab64911ab72d0a05e930fb06dc
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-06 20:19:20 -0500

    used this increment and decrement function

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ class App extends Component {
     this.state = {
       count: 0
     }
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    
+  }
+  increment (){
+    this.setState({count: this.state.count + 1})
+  }
+  decrement (){
+    this.setState({count: this.state.count - 1})
   }
   render() {
     return (
     <div>
-    <button onClick = {() => this.setState({count: this.state.count + 1})}>
+    <button onClick = {this.increment}>
       +
     </button>
-    <button onClick = {() => this.setState({count: this.state.count - 1})}> -
+    <button onClick = {this.decrement}> -
     </button>
     <h1>
       {this.state.count}
